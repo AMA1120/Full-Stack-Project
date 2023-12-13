52e69fedfd055655df53151ceaedc73eb8a67502
Pallimulla K Imansha <89239162+imansha27@users.noreply.github.com>
2023-12-13 11:10:57 +0530

    mongodb database url added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
 
-app.listen(5000, () => {
+const DB_URL = 'mongodb+srv://pkkimansha27:resturant123@resturant.c1gnqtq.mongodb.net/?retryWrites=true&w=majority'
+app.listen(4000, () => {
     console.log("Server Started");
   });
+
+  mongoose.connect(DB_URL)
+  .then(() =>{
+    console.log('DB Connected');
+
+  })
+  .catch((err)=> console.log('DB connection error,err'));
