6a920d7bd86eaadaaf4a4d94270b8c53d448b67a
Ama Weerasinghe <manweerasinghe@students.nsbm.ac.lk>
2023-12-14 11:02:03 +0530

    Update Promotions.js

--- dashboard/src/pages/promotions/Promotions.js
+++ dashboard/src/pages/promotions/Promotions.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import Navbar from "../../components/navbar/navbar";
-import './promotions.css';
+// import React, { useState } from 'react'
+// // import Navbar from "../../components/navbar/navbar";
+// import './promotions.css';
 
-function Promotions() {
-  return (
-    <div className="promo-container">
-      <Navbar />
-      <div className="promo-content">
-        {/* Your home content goes here */}
-        <h1>Welcome to Your Website</h1>
-        <p>This is the main content of your promotions page.</p>
-      </div>
-    </div>
-  )
-}
+// function Promotions() {
 
-export default Promotions
+//   const[image,setImage] = useState("");
+
+//   function convertToBase64(e){
+//     console.log(e);
+//     var reader = new FileReader();
+//     reader.readAsDataURL(e.target.files[0]);
+//     reader.onload = function () {
+//       console.log(reader.result);
+//       setImage(reader.result);
+//     };
+//     reader.onerror = function (error) {
+//       console.log('Error: ', error);
+//     };
+//   }
+//   return (
+//     <div className="promo-container">
+//       {/* <Navbar /> */}
+//       <div className="promo-content">
+//        <div className='auth-wrapper'>
+//         <div className='auth-inner' style={{ width: "auto"}}>
+//           Let's Upload Image <br/>
+//           <input 
+//             accept='image/*'
+//             type ="file"
+//             onChange={convertToBase64}
+//          />
+//          {image===""|| image==null?"": <img width={100} height={100} src={image}/>}
+//         </div>
+//        </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Promotions
