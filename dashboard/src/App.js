3f806d66c9a8848f7e5d1177a186256e8c6beef9
Ama Weerasinghe <manweerasinghe@students.nsbm.ac.lk>
2023-12-18 11:44:21 +0530

    add promotion done

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Food from './pages/food/Food';
-import Promotions from './pages/promotions/Promotions';
+import AddPromotions from './pages/promotions/AddPromotions';
 import Login from './pages/login/Login';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateFood from './pages/food/CreateFood'
 import UpdateFood from './pages/food/UpdateFood'
+import HomePromotions from './pages/promotions/HomePromotions';
 
 function App() {
   const [count, setCount]=useState(0)
@@ -19,10 +20,11 @@ function App() {
           <Route index element={<Home />}/>
           <Route path="/home" element={<Home />}/>
           <Route path="/food" element={<Food />}/>
-          <Route path="/promotions" element={<Promotions />}/>
+          <Route path="/promotions" element={<AddPromotions />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/create" element={<CreateFood />} />
           <Route path="/update" element={<UpdateFood />}/>
+          <Route path="/homepromotions" element={<HomePromotions/>}/>
         </Routes>
       </BrowserRouter>
       
