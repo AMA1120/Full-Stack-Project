977adfeecfca116cf6edbff36ab735c2a4da6a9c
Ama Weerasinghe <manweerasinghe@students.nsbm.ac.lk>
2023-12-18 10:49:27 +0530

    Promotion crud changes

--- dashboard/src/pages/promotions/Promotions.js
+++ dashboard/src/pages/promotions/Promotions.js
@@ -42,34 +42,58 @@ function Promotions() {
     <div className="promo-container">
       <Navbar/>
       <div className='promo-content' >
-      <h1>Upload Image</h1>
-      <input accept="image/*" type="file" onChange={convertToBase64} />
-      {image === "" || image == null ? (
-        ""
-      ) : (
-        <img width={100} height={100} src={image} alt="" />
-      )}
-      Name:{" "}
-      <input
-        type="text"
-        value={promotionName}
-        onChange={(e) => setpromotionName(e.target.value)}
-      />
-       description:{" "}
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-       category:{" "}
-      <input
-        type="text"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      />
-      <button onClick={uploadImage}>Upload Image</button>
+       <div className="d-flex justify-content-center align-items-center">
+            <div className="custom-container">
+            <form onSubmit={uploadImage}>
+              <h1>Add Promotions</h1>
+              <div className="mb-2">
+              <label htmlFor="image">Image</label>
+              <br></br>
+              <input accept="image/*" 
+              type="file"
+              onChange={convertToBase64} />
+              {image === "" || image == null ? (
+                ""
+              ) : (
+                <img width={100} height={100} src={image} alt="" />
+              )}
       </div>
+               <div className="mb-2">
+                  <label htmlFor="Promotion">Promotion</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Promotion Name"
+                    className="form-control"
+                    value={promotionName}
+                    onChange={(e) => setpromotionName(e.target.value)}
+                  />
+                  </div>
+                  <div className="mb-2">
+                  <label htmlFor="discription">Discription</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Discription"
+                    className="form-control"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="mb-2">
+                  <label htmlFor="food_category">category</label>
+                  <input
+                    type="text"
+                    placeholder="Enter promotion category"
+                    className="form-control"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                  />
+                </div>
+           <button className="btn btn-success" onClick={uploadImage}>Submit</button>
+      </form>
+      </div>
+     </div>
     </div>
+  </div>
   );
 }
 
