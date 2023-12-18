b5258f4bd9dc31e33940fe72ac8978cdc0031c13
Ama Weerasinghe <manweerasinghe@students.nsbm.ac.lk>
2023-12-18 11:50:07 +0530

    Update HomePromotions.js

--- dashboard/src/pages/promotions/HomePromotions.js
+++ dashboard/src/pages/promotions/HomePromotions.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './promotions.css';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar/navbar';
-import AddPromotions from './AddPromotions';
 
 export default function HomePromotions() {
   return (
