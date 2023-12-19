d35b793d7d257302a3e8d7d6102d9dce02a133cd
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2023-12-19 19:45:28 +0530

    Update Menuitem.js

--- client/src/pages/Menuitem/Menuitem.js
+++ client/src/pages/Menuitem/Menuitem.js
@@ -13,14 +13,6 @@ export default function Menuitem() {
 
       <br></br>
       <br></br>
-
-      <div className='carousel-caption'>
-      <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-      
-      </div>
       
       <h3>Menu Items</h3>
       <br></br>
