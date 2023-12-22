c16941a94fb2f3615e698ca04aac7de27dcf1156
Ama Weerasinghe <manweerasinghe@students.nsbm.ac.lk>
2023-12-22 11:12:04 +0530

    Promotion
    changes in client-side promotion

--- client/src/components/Promotion/Promotion.js
+++ client/src/components/Promotion/Promotion.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Promotion.css";
 
 
 function Promotion() {
@@ -20,19 +22,27 @@ function Promotion() {
 
   return (
     <div>
+      <div className="promo-card-container">
       {promotions.map((promotion) => (
         <div key={promotion._id} className="promo-card">
           <div className="promo-card-body">
             {promotion.image && (
               <img
                 src={promotion.image}
-                alt={`Food: ${promotion.food_item}`}
-                className="promo-image"
+                alt={`promotion: ${promotion.promotion_name}`}
+                className="promo-client-image"
               />
             )}
+            
+             <div className="promo-card-footer">
+                    <Link to={`/menu/${promotion._id}`} className="order-button">
+                      Order Now
+                    </Link>
+              </div>
           </div>
         </div>
       ))}
+      </div>
     </div>
   );
 }
