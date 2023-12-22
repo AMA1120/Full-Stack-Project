525c7a86977278dc5a575fed5c07dc39e030c677
Bihari Layanjali <115726378+Bihari83@users.noreply.github.com>
2023-12-22 09:17:24 +0530

    Changes in menuitem.css

--- client/src/pages/Menuitem/Menuitem.js
+++ client/src/pages/Menuitem/Menuitem.js
@@ -3,7 +3,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import Card from '../../components/Card/Card';
 import Footer from '../../components/Footer/Footer';
 
-import './menuitem.css';
+//import './menuitem.css';
 
 export default function Menuitem() {
   return (
