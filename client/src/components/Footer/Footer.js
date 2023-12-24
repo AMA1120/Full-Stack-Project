610fbfd3e546b69eac71c70004660162191f0ae6
Rumeesha Gunarathna <116654587+rumeeshagunarathna@users.noreply.github.com>
2023-12-24 13:32:42 +0530

    Added

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,21 +1,90 @@
-import React from 'react'
-//import './footer.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPhoneVolume,
+  FaEnvelope,
+  FaPaperPlane,
+} from "react-icons/fa";
+import "./footer.css";
+import logoImage from "../../Images/logo2.jpeg"; // Import your logo image
 
 function Footer() {
-  return (
-    <div>
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <Link to="/" className="mb-3 me-2 mb-md-0 text-mutedk text-decoration-none lh-1">
-            
-          </Link>
-          <span className="text-muted">© 2023 FullStack, Inc</span>
-        </div>
+  const iconStyle = { color: "yellow" }; // Set the color to yellow
 
-      </footer>
-    </div>
-  )
+  return (
+    <>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-lg-5 col-12 ft-1">
+              <h3>
+                <img src={logoImage} alt="Logo" className="logo" />
+              </h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Laborum ea quo ex ullam laboriosam magni totam, facere eos iure
+                voluptate.
+              </p>
+              <div className="footer-icons">
+                <FaFacebook className="icon" />
+                <FaTwitter className="icon" />
+                <FaInstagram className="icon" />
+                <FaLinkedinIn className="icon" />
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-3 col-12 ft-2">
+              <h5><u>Quick Links</u></h5>
+              <ul>
+                <li className="nav-item">
+                  <a className="" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/menu/:foodID">
+                    Our Menu
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/cart">
+                    View Cart
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/userprofile">
+                    Profile
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="" href="/login">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 col-lg-4 col-12 ft-3">
+              <h5><u>Contact Us</u></h5>
+              <p>
+                <FaPhoneVolume style={iconStyle} /> +94 345627892
+              </p>
+              <p>
+                <FaEnvelope style={iconStyle} /> fullstack@gmail.com
+              </p>
+              <p>
+                <FaPaperPlane style={iconStyle} /> Homagama, Sri Lanka.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Last-footer">
+        <p>copyright@2023:Design by Group 34</p>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./navbar.css";
 import { Link, useNavigate } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import logoImage from "../../Images/logo2.jpeg"; // Import your logo image
 
 const Navbar = () => {
   const token = localStorage.getItem("token");
@@ -13,21 +15,53 @@ const Navbar = () => {
 
   return (
     <nav>
+      <div className="logo-container">
+        <img src={logoImage} alt="Logo" className="logo" />
+      </div>
+
       {token ? (
         <ul className="nav-ul">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/menu/:foodID">Menu Items</Link></li>
-          <li><Link to="/cart">My Cart</Link></li>
-          <li><Link to="/cart">My Orders</Link></li>
-          <li><Link to="/userprofile">Userprofile</Link></li>
-          <li><div className="logout-link" onClick={logout}>Logout</div></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/menu/:foodID">Menu Items</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <FaShoppingCart />
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart">My Orders</Link>
+          </li>
+          <li>
+            <Link to="/userprofile">Userprofile</Link>
+          </li>
+          <li>
+            <div className="logout-link" onClick={logout}>
+              Logout
+            </div>
+          </li>
         </ul>
       ) : (
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/menu/:foodID">Menu Items</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
-          <li><Link to="/login">Login</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/menu/:foodID">Menu Items</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <FaShoppingCart />
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className="login-button">
+              Login
+            </Link>
+          </li>
         </ul>
       )}
     </nav>
