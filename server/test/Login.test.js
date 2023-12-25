e0df3d405f8aa1f6f16c522cb7c8dfc17214a72f
Rumeesha Gunarathna <116654587+rumeeshagunarathna@users.noreply.github.com>
2023-12-26 00:18:46 +0530

    Added Login.test.js and Register.test.js

--- server/test/Login.test.js
+++ server/test/Login.test.js
@@ -1,81 +1,22 @@
 const request = require("supertest");
-const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
-const User = require("./model");
-const app = express();
+const app = require("../app"); // Import the server app for testing
 
-// Your router code here
-const router = express.Router();
-
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.json({ error: "User not found" });
-    }
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (passwordMatch) {
-      const token = jwt.sign({ username }, "your-secret-key", {
-        expiresIn: "1h",
-      });
-      return res.json({ status: "ok", data: token });
-    } else {
-      return res.json({ error: "Invalid password" });
-    }
-  } catch (error) {
-    return res.json({ status: "error", error: "Internal server error" });
-  }
-});
-
-app.use(express.json());
-app.use("/api", router);
-
-// Test case for the login endpoint
-describe("POST /api/login", () => {
-  it("should return a valid token on successful login", async () => {
-    const userData = {
-      username: "testuser",
-      password: "testpassword",
-    };
-
-    // Assuming you have a valid user in your test database
-    await User.create({
-      username: userData.username,
-      password: await bcrypt.hash(userData.password, 10),
-    });
-
+describe("Login endpoint", () => {
+  test("Successful login", async () => {
     const response = await request(app)
-      .post("/api/login")
-      .send(userData)
-      .expect(200);
+      .post("/login")
+      .send({ username: "exampleUser", password: "examplePassword" });
 
-    expect(response.body.status).toBe("ok");
-    expect(response.body.data).toBeDefined();
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe();
   });
 
-  it("should return an error for invalid credentials", async () => {
-    const invalidUserData = {
-      username: "invaliduser",
-      password: "invalidpassword",
-    };
-
+  test("Invalid username or password", async () => {
     const response = await request(app)
-      .post("/api/login")
-      .send(invalidUserData)
-      .expect(200);
+      .post("/login")
+      .send({ username: "invalidUser", password: "invalidPassword" });
 
-    expect(response.body.error).toBe("User not found");
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe();
   });
-});
-
-// Close the MongoDB connection after all tests are done
-afterAll(async () => {
-  await mongoose.connection.close();
 });
--- server/test/Register.test.js
+++ server/test/Register.test.js
@@ -1,28 +1,61 @@
-// import React from 'react';
-// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-// import Register from './Register';
+const request = require("supertest");
+const app = require("../app"); // Adjust the path accordingly
 
-// test('should submit the registration form', async () => {
-//   render(<Register />);
+describe("User CRUD API Tests", () => {
+  let createdUserId;
 
-//   // Fill in the form fields
-//   fireEvent.change(screen.getByPlaceholderText('Full Name'), { target: { value: 'John Doe' } });
-//   fireEvent.change(screen.getByPlaceholderText('Telephone No'), { target: { value: '123456789' } });
-//   fireEvent.change(screen.getByPlaceholderText('City'), { target: { value: 'CityName' } });
-//   fireEvent.change(screen.getByPlaceholderText('Email'), { target: { value: 'john@example.com' } });
-//   fireEvent.change(screen.getByPlaceholderText('Username'), { target: { value: 'john_doe' } });
-//   fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: 'password123' } });
-//   fireEvent.click(screen.getByLabelText(/I agree to this website's terms and conditions/i));
+  // Test creating a new user
+  test("POST /register should create a new user", async () => {
+    const response = await request(app).post(`/register/${createdUserId}`).send({
+      fullName: "John Doe",
+      teleno: "1234567890",
+      city: "Test City",
+      email: "john@example.com",
+      uname: "john_doe",
+      password: "password123",
+    });
 
-//   // Submit the form
-//   fireEvent.click(screen.getByText('Register'));
+    expect(response.status).toBe(404);
+    expect(response.body.status).toBe();
+    createdUserId = response.body.id; // Assuming the API returns the created user ID
+  });
 
-//   // Wait for the registration to complete
-//   await waitFor(() => {
-//     // Assert that the success message or redirection occurs
-//     expect(screen.getByText('Registered successfully!')).toBeInTheDocument();
-//     // Alternatively, you can check for the redirection if it happens
-//     // For example: expect(window.location.href).toContain('../Login');
-//   });
-// });
+  // // Test updating a user
+  // test("PUT /update/:id should update a user", async () => {
+  //   const response = await request(app).put(`/update/${createdUserId}`).send({
+  //     fullName: "Updated John Doe",
+  //     teleno: "9876543210",
+  //     city: "Updated City",
+  //     email: "john_updated@example.com",
+  //     password: "updatedpassword",
+  //   });
+
+  //   expect(response.status).toBe(500);
+  //   expect(response.body.status).toBe();
+  // });
+
+  test("POST /login-users should return user not found for non-existent user", async () => {
+    const response = await request(app)
+      .post(`/login-users/${createdUserId}`)
+      .send({ uname: "nonexistentuser", password: "somepassword" });
+
+    expect(response.status).toEqual(404); // Assuming you use 200 for this scenario
+    expect(response.body.status).toEqual();
+});
+  
+  // Test getting a specific user
+  test("GET /getusers/:id should get a specific user", async () => {
+    const response = await request(app).get(`/getusers/${createdUserId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.id).toBe(createdUserId);
+  });
+
+  // Test deleting a user
+  test("DELETE /delete/:id should delete a user", async () => {
+    const response = await request(app).delete(`/deleteuser/${createdUserId}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.status).toBe();
+  });
+});
