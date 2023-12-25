9b3bf5660b6613d81a4a05f87eef76d9f91e45d4
Rumeesha Gunarathna <116654587+rumeeshagunarathna@users.noreply.github.com>
2023-12-25 14:16:44 +0530

    Added

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const FoodcrudModel = require("./models/Foodcrud");
+
+//import routes
+const userRoutes = require("./routes/users");
+const promotionRoutes = require("./routes/promotion");
+const foodcrudRoutes = require("./routes/foodcrud");
+const menuRoutes = require("./routes/menuroute");
+
+app.use(cors());
+app.use(express.json());
+
+//app middleware
+app.use(bodyParser.json({ limit: "50mb" }));
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
+//route middleware
+app.use(userRoutes);
+app.use(promotionRoutes);
+app.use(foodcrudRoutes);
+app.use(menuRoutes);
+
+module.exports = app;
