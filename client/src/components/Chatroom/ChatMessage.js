9f7a3c0c0df646e99f9d6c090ec88275e8dd41ce
Pallimulla K Imansha <89239162+imansha27@users.noreply.github.com>
2023-12-30 19:32:11 +0530

    chatroom done

--- client/src/components/Chatroom/ChatMessage.js
+++ client/src/components/Chatroom/ChatMessage.js
@@ -1,6 +1,23 @@
-import React from 'react'
+// ChatMessage.jsx
 
-export default ({ name, message }) =>
-  <p>
-    <strong>{name}</strong> <em>{message}</em>
-  </p>
+import React from "react";
+import "./Chat.css";
+
+const ChatMessage = ({ name, message, currentUser, timestamp }) => {
+  const isCurrentUser = name === currentUser;
+  const messageClass = isCurrentUser
+    ? "current-user-message"
+    : "other-user-message";
+
+  return (
+    <div className={`chat-message ${messageClass}`}>
+      <div className="chat-message-header">
+        <strong>{name}</strong>
+        <span className="message-timestamp">{timestamp}</span>
+      </div>
+      <div className="chat-message-body">{message}</div>
+    </div>
+  );
+};
+
+export default ChatMessage;
